40272704c4ae059b11285ecaaa39a2100954002a
feduuk <scharov.fedya@yandex.ru>
2021-09-15 21:44:00 +0300

    fixed error

--- web/src/main/java/com/web/DefineFriendsController.java
+++ web/src/main/java/com/web/DefineFriendsController.java
@@ -34,61 +34,11 @@ public class DefineFriendsController {
         this.workerService = workerService;
     }
 
-//    @GetMapping("/defineFriends")
-//    public String defineFriends(Model model){
-//        //List<String> activities = groupRepository.findActivities();
-//        String token = "e8d5e2366e9cca21f7c83ba93088e7118efd53f8a01f2576d94b3b8e6127b0d522c75a496eb4e8ff638b4";
-//        friendRepository.saveAll(workerService.getFriendsFromVk(token));
-//        List<String> activities = new ArrayList<>(Arrays.asList("Автомобили",
-//                "Активный отдых",
-//                "Бизнес",
-//                "Видеоигры",
-//                "Дизайн и графика",
-//                "Домашние и дикие животные",
-//                "Здоровый образ жизни",
-//                "Искусство и развлечения",
-//                "История",
-//                "Киберспорт",
-//                "Кино",
-//                "Кулинария",
-//                "Кулинария, рецепты",
-//                "Культура",
-//                "Литература",
-//                "Музыка",
-//                "Наука",
-//                "Политика",
-//                "Программирование",
-//                "Творчество",
-//                "Туризм, путешествия",
-//                "Философия",
-//                "Юмор"
-//        ));
-//
-//        FriendModel friendModel = new FriendModel();
-//        model.addAttribute("activities", activities);
-//        model.addAttribute("friendModel", friendModel);
-//        return "defineFriends";
-//    }
-
-
-
-
-    @GetMapping(value="/processCode")
-    public String processCode(@RequestParam String code){
-        log.info("!defineFriends");
-        response = restTemplate.getForObject(String.format("https://oauth.vk.com/access_token?client_id=7914980&client_secret=FQQsAPkXfHP3D2MhRHin&redirect_uri=http://34.136.208.204:8080/processCode&code=%s", code), VkResponse.class);
-        return "redirect:defineFriends";
-    }
-
-    @GetMapping(value="/defineFriends")
+    @GetMapping("/defineFriends")
     public String defineFriends(Model model){
-        log.info("access_token: " + response.getAccess_token());
-        log.info("expires_in: " + response.getExpires_in());
-        log.info("user_id: " + response.getUser_id());
-
-        friendRepository.saveAll(workerService.getFriendsFromVk(response.getAccess_token()));
-
         //List<String> activities = groupRepository.findActivities();
+        String token = "e8d5e2366e9cca21f7c83ba93088e7118efd53f8a01f2576d94b3b8e6127b0d522c75a496eb4e8ff638b4";
+        friendRepository.saveAll(workerService.getFriendsFromVk(token));
         List<String> activities = new ArrayList<>(Arrays.asList("Автомобили",
                 "Активный отдых",
                 "Бизнес",
@@ -117,9 +67,59 @@ public class DefineFriendsController {
         FriendModel friendModel = new FriendModel();
         model.addAttribute("activities", activities);
         model.addAttribute("friendModel", friendModel);
-        model.addAttribute("vkReference", String.format("https://vk.com/id%s", response.getUser_id()));
         return "defineFriends";
     }
+
+
+
+
+//    @GetMapping(value="/processCode")
+//    public String processCode(@RequestParam String code){
+//        log.info("!defineFriends");
+//        response = restTemplate.getForObject(String.format("https://oauth.vk.com/access_token?client_id=7914980&client_secret=FQQsAPkXfHP3D2MhRHin&redirect_uri=http://34.136.208.204:8080/processCode&code=%s", code), VkResponse.class);
+//        return "redirect:defineFriends";
+//    }
+//
+//    @GetMapping(value="/defineFriends")
+//    public String defineFriends(Model model){
+//        log.info("access_token: " + response.getAccess_token());
+//        log.info("expires_in: " + response.getExpires_in());
+//        log.info("user_id: " + response.getUser_id());
+//
+//        friendRepository.saveAll(workerService.getFriendsFromVk(response.getAccess_token()));
+//
+//        //List<String> activities = groupRepository.findActivities();
+//        List<String> activities = new ArrayList<>(Arrays.asList("Автомобили",
+//                "Активный отдых",
+//                "Бизнес",
+//                "Видеоигры",
+//                "Дизайн и графика",
+//                "Домашние и дикие животные",
+//                "Здоровый образ жизни",
+//                "Искусство и развлечения",
+//                "История",
+//                "Киберспорт",
+//                "Кино",
+//                "Кулинария",
+//                "Кулинария, рецепты",
+//                "Культура",
+//                "Литература",
+//                "Музыка",
+//                "Наука",
+//                "Политика",
+//                "Программирование",
+//                "Творчество",
+//                "Туризм, путешествия",
+//                "Философия",
+//                "Юмор"
+//        ));
+//
+//        FriendModel friendModel = new FriendModel();
+//        model.addAttribute("activities", activities);
+//        model.addAttribute("friendModel", friendModel);
+//        model.addAttribute("vkReference", String.format("https://vk.com/id%s", response.getUser_id()));
+//        return "defineFriends";
+//    }
 
     @PostMapping("/defineFriends")
     public String processFriends(FriendModel friendModel){
--- web/src/main/java/com/web/WorkerService.java
+++ web/src/main/java/com/web/WorkerService.java
@@ -60,12 +60,11 @@ public class WorkerService {
                     List<Group> groups = null;
                     Friend friendInDb = friendRepository.findById(friend.getId());
                     if (friendInDb == null || getUpdatedTimeDiffForCurrentFriend(friendInDb) > 24) {
-                        URL url2 = new URL(String.format("https://api.vk.com/method/users.getSubscriptions?fields=activity,description&user_id=%s&extended=1&v=5.52&access_token=%s", friend.getId(), token));
+                        URL url2 = new URL(String.format("https://api.vk.com/method/users.getSubscriptions?fields=activity,description&user_id=%s&extended=1&v=5.100&access_token=%s", friend.getId(), token));
                         JsonNode jsonNode2 = objectMapper.readTree(url2);
                         JsonNode jn = jsonNode2.get("response");
                         if (jn != null) {
-                            groups = objectMapper.convertValue(jn.get("items"), new TypeReference<List<Group>>() {
-                            });
+                            groups = objectMapper.convertValue(jn.get("items"), new TypeReference<List<Group>>() {});
                             friend.setGroups(groups);
                         }
                         friend.setUpdateDate(new Date());
@@ -86,6 +85,11 @@ public class WorkerService {
     }
 
 
+
+
+
+
+
     public List<Friend> getFriendsFromVkWithoutDB(){
         List<Friend> friends = null;
         ObjectMapper objectMapper = new ObjectMapper();
@@ -94,7 +98,7 @@ public class WorkerService {
         URL url2 = null;
         List<Group> groups = null;
         try {
-            url1 = new URL("https://api.vk.com/method/friends.get?count=20&fields=city,country,sex,movies,music,occupation,relation,activities,bdate,career,activities,interests,music,movies,tv,books,games,about&v=5.52&access_token=e8d5e2366e9cca21f7c83ba93088e7118efd53f8a01f2576d94b3b8e6127b0d522c75a496eb4e8ff638b4");
+            url1 = new URL("https://api.vk.com/method/friends.get?count=20&fields=city,country,sex,movies,music,occupation,relation,activities,bdate,career,activities,interests,music,movies,tv,books,games,about&v=5.100&access_token=e8d5e2366e9cca21f7c83ba93088e7118efd53f8a01f2576d94b3b8e6127b0d522c75a496eb4e8ff638b4");
             JsonNode jsonNode1 = objectMapper.readTree(url1);
             String str1 = jsonNode1.get("response").get("items").toString();
             friends = objectMapper.readValue(str1, new TypeReference<List<Friend>>(){});
@@ -104,7 +108,7 @@ public class WorkerService {
 
             for (Friend friend : friends) {
                 user_id = friend.getId();
-                url2 = new URL(String.format("https://api.vk.com/method/users.getSubscriptions?fields=activity,description&user_id=%s&extended=1&v=5.52&access_token=e8d5e2366e9cca21f7c83ba93088e7118efd53f8a01f2576d94b3b8e6127b0d522c75a496eb4e8ff638b4", user_id));
+                url2 = new URL(String.format("https://api.vk.com/method/users.getSubscriptions?fields=activity,description&user_id=%s&extended=1&v=5.100&access_token=e8d5e2366e9cca21f7c83ba93088e7118efd53f8a01f2576d94b3b8e6127b0d522c75a496eb4e8ff638b4", user_id));
                 jsonNode2 = objectMapper.readTree(url2);
                 String str2 = null;
                 JsonNode jn = jsonNode2.get("response");
